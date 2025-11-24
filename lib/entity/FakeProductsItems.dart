483792a2878092ac6d5db3d8435ab4f70fbe5b7e
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-11-24 12:54:52 +0200

    feature/add FakeProductsItems to hold all products

---
+++ lib/entity/FakeProductsItems.dart
@@ -0,0 +1,41 @@
+import 'package:jerry_store/entity/ProductInfo.dart';
+
+List<ProductInfo> productsItems = [
+  ProductInfo(
+    title: 'Sport Tom',
+    image: 'tom_sport',
+    description: 'He runs 1 meter... trips over his boot.',
+    price: 5,
+    discount: 3,
+  ),
+  ProductInfo(
+    title: 'Tom the bomb',
+    image: 'tom_bomb',
+    description: 'He blows himself up before Jerry can catch him.',
+    price: 10,
+  ),
+  ProductInfo(
+    title: 'Spy Tom',
+    image: 'tom_spy',
+    description: 'Disguises itself as a table.',
+    price: 12,
+  ),
+  ProductInfo(
+    title: 'Frozen Tom',
+    image: 'tom_frozen',
+    description: 'He was chasing Jerry, he froze after the first look',
+    price: 10,
+  ),
+  ProductInfo(
+    title: 'Tom the lover',
+    image: 'tom_lover',
+    description: 'He loves one-sidedly... and is beaten by the other side.',
+    price: 5,
+  ),
+  ProductInfo(
+    title: 'Sleeping Tom',
+    image: 'tom_sleeping',
+    description: "He doesn't chase anyone, he just snores in stereo.",
+    price: 10,
+  ),
+];
