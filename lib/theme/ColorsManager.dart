b64e526952a1c5b9cf7cf46013683b98bd2eb37b
MahmoodTarek <dev.mahmoudtarek@gmail.com>
2025-11-24 12:52:16 +0200

    feature/add ColorsManager to hold all the app colors

---
+++ lib/theme/ColorsManager.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color blueDark = Color(0xFF03578A);
+  static const Color blueMid = Color(0xFF0685D0);
+  static const Color blueSky = Color(0xFFE9F6FB);
+  static const Color blueLight = Color(0xFFEEF4F6);
+
+  static const Color grayDark = Color(0xFF1F1F1E);
+  static const Color grayLight = Color(0xFF969799);
+  static const Color grayMid = Color(0xFF969799);
+}
